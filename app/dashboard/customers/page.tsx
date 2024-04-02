d96ffaf7d4ff0b6218361ed5dc86be7a234dc3d6
gupta123456 <anujg0046@gmail.com>
2024-04-02 19:08:00 +0530

    updated code

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,33 @@
-export default function Customers() {
-    return <p>Customers Page</p>;
-  }
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search Customers..." />
+      </div>
+      <Suspense key={query}>
+        <Table query={query} customers={[]} />
+      </Suspense>
+    </div>
+  );
+}
